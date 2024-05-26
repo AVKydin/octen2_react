0e1cfe01d9500f259bf1b7a099dd4c5d82dd5754
AVKydin <tlalok@ukr.net>
2024-05-26 11:53:33 +0300

    L03 usePrevious

---
+++ src/hooks/usePrevious.ts
@@ -0,0 +1,14 @@
+import {useRef} from "react";
+
+const usePrevious = (value:any) => {
+    const currentRef = useRef(value);
+
+    const previousRef = useRef()
+
+    if(currentRef.current !== value){
+        previousRef.current = currentRef.current
+    }
+
+    return previousRef.current
+
+}
