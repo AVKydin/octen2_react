921087d0c9aff9143d3d16df2b636bb66e566d86
AVKydin <tlalok@ukr.net>
2024-06-11 22:02:39 +0300

    L10 end

--- src/components/PaginationComponent.tsx
+++ src/components/PaginationComponent.tsx
@@ -1,27 +1,30 @@
-import React, {FC} from 'react';
-import {IPaginatedPageModel} from "../models/IPaginatedPageModel";
-
+import React, { FC } from 'react';
+import { IPaginatedPageModel } from "../models/IPaginatedPageModel";
+import { URLSearchParamsInit } from "react-router-dom";
 
 interface IProps {
-    onChangePage: (action: string) => void;
     prev: IPaginatedPageModel | null;
     next: IPaginatedPageModel | null;
+    setQuery: (nextInit: URLSearchParamsInit, navigateOptions?: { replace?: boolean | undefined; state?: any; }) => void;
 }
 
-const PaginationComponent:FC<IProps> = ({onChangePage, prev, next}) => {
+const PaginationComponent: FC<IProps> = ({ prev, next, setQuery }) => {
 
-
-
-
+    const onChangePage = (action: string) => {
+        switch (action) {
+            case "prev":
+                if (prev) setQuery({ page: prev.page });
+                break;
+            case "next":
+                if (next) setQuery({ page: next.page });
+                break;
+        }
+    };
 
     return (
         <div>
-            <button onClick={() => {
-                onChangePage('prev')
-            }} disabled={!prev}>prev</button>
-            <button onClick={() => {
-                onChangePage('next')
-            }} disabled={!next}>next</button>
+            <button onClick={() => onChangePage('prev')} disabled={!prev}>prev</button>
+            <button onClick={() => onChangePage('next')} disabled={!next}>next</button>
         </div>
     );
 };
--- src/pages/CarsPage.tsx
+++ src/pages/CarsPage.tsx
@@ -1,13 +1,12 @@
-import React, {useEffect, useState} from 'react';
-import {carService} from "../services/api.service";
+import React, { useEffect, useState } from 'react';
+import { carService } from "../services/api.service";
 import CarsComponent from "../components/CarsComponent";
-import {ICarModel} from "../models/ICarModel";
+import { ICarModel } from "../models/ICarModel";
 import PaginationComponent from "../components/PaginationComponent";
-import {useSearchParams} from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 const CarsPage = () => {
-
-    const [query, setQuery] = useSearchParams({page: '1'});
+    const [query, setQuery] = useSearchParams({ page: '1' });
 
     const [carsPaginationObject, setCarsPaginationObject] = useState<ICarModel>({
         items: [],
@@ -15,32 +14,24 @@ const CarsPage = () => {
         prev: null,
         total_items: 0,
         total_pages: 0
-    })
+    });
 
-        useEffect(() => {
-                carService.getAllCars(query.get('page') || '1').then(value => {
-                    if (value) {
-                        setCarsPaginationObject(value)
-                    }
-                })
-        }, [query]);
-
-    const onChangePage = (action: string) => {
-        switch (action) {
-            case "prev":
-                setQuery({...carsPaginationObject.prev})
-                break;
-            case "next":
-                setQuery({...carsPaginationObject.next})
-                break;
-
-        }
-    }
+    useEffect(() => {
+        carService.getAllCars(query.get('page') || '1').then(value => {
+            if (value) {
+                setCarsPaginationObject(value);
+            }
+        });
+    }, [query]);
 
     return (
         <div>
-            <CarsComponent cars={carsPaginationObject.items}/>
-            <PaginationComponent next={carsPaginationObject.next} prev={carsPaginationObject.prev} onChangePage={onChangePage}/>
+            <CarsComponent cars={carsPaginationObject.items} />
+            <PaginationComponent
+                setQuery={setQuery}
+                next={carsPaginationObject.next}
+                prev={carsPaginationObject.prev}
+            />
         </div>
     );
 };
