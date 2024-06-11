360d176d37059b6d3f84413c5c4acc0ba8435915
AVKydin <tlalok@ukr.net>
2024-06-11 22:27:25 +0300

    L11 init

---
+++ src/router/router.tsx
@@ -0,0 +1,23 @@
+import {createBrowserRouter, RouteObject} from "react-router-dom";
+import App from "../App";
+import UsersPage from "../pages/UsersPage";
+import UserPostsPage from "../pages/UserPostsPage";
+import PostsPage from "../pages/PostsPage";
+
+const routes: RouteObject[] = [
+    {
+        path: '', element: <App/>, children: [
+            {
+                path:'users', element: <UsersPage/>
+            },
+            {
+                path:'posts', element: <PostsPage/>
+            },
+            {
+                path:'userPosts', element: <UserPostsPage/>
+            }
+        ]
+    }
+]
+
+export const router = createBrowserRouter(routes)
